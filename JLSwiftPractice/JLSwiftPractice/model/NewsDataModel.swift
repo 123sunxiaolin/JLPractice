2099039c588e9f20f044d8b3b98c7418e5e16f12
jacklin <401788217@qq.com>
2017-09-17 22:51:27 +0800

    [add] add test file

---
+++ JLSwiftPractice/JLSwiftPractice/model/NewsDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewsDataModel.swift
+//  JLSwiftPractice
+//
+//  Created by jacklin on 2017/9/17.
+//  Copyright © 2017年 jacklin. All rights reserved.
+//
+
+import UIKit
+
+class NewsDataModel: NSObject {
+
+}
---
+++ JLSwiftPractice/JLSwiftPractice/model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  JLSwiftPractice
+//
+//  Created by jacklin on 2017/9/17.
+//  Copyright © 2017年 jacklin. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
