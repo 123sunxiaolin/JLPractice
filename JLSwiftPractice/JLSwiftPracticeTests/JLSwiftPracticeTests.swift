7288d9baf5b33b6743443b8291bc7257ec8575a3
jacklin <401788217@qq.com>
2017-09-17 11:10:01 +0800

    [add] add pure swift support project

---
+++ JLSwiftPractice/JLSwiftPracticeTests/JLSwiftPracticeTests.swift
@@ -0,0 +1,36 @@
+//
+//  JLSwiftPracticeTests.swift
+//  JLSwiftPracticeTests
+//
+//  Created by jacklin on 2017/9/17.
+//  Copyright © 2017年 jacklin. All rights reserved.
+//
+
+import XCTest
+@testable import JLSwiftPractice
+
+class JLSwiftPracticeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
