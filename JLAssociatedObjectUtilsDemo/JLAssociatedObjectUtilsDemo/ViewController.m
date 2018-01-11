2132250794b6b0455503054b986da94f4c7713ea
jacklin <401788217@qq.com>
2018-01-12 00:58:02 +0800

    Add sample code for `JLAssociatedObjectUtils`

--- JLAssociatedObjectUtilsDemo/JLAssociatedObjectUtilsDemo/ViewController.m
+++ JLAssociatedObjectUtilsDemo/JLAssociatedObjectUtilsDemo/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "JLAssociatedObjectUtils.h"
+
+static NSString *const kJLActionHandlerTapGestureKey = @"JLActionHandlerTapGestureKey";
+static NSString *const kJLActionHandlerTapBlockKey = @"JLActionHandlerTapBlocKey";
 
 @interface ViewController ()
 
@@ -19,6 +23,33 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - For example
+- (void)setTapActionWithBlock:(void (^)(void))block
+{
+    UITapGestureRecognizer *tapGR = [JLAssociatedObjectUtils JL_getAssociatedObject:self key:kJLActionHandlerTapGestureKey];
+    
+    if (!tapGR)
+    {
+        tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(JL_handleActionForTapGesture:)];
+        [self.view addGestureRecognizer: tapGR];
+        [JLAssociatedObjectUtils JL_setAssociatedObject:self key:kJLActionHandlerTapGestureKey value:tapGR policy:JLAssociationPolicyRetain];
+    }
+
+     [JLAssociatedObjectUtils JL_setAssociatedObject:self key:kJLActionHandlerTapBlockKey value:tapGR policy:JLAssociationPolicyCopy];
+}
+
+- (void) JL_handleActionForTapGesture:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateRecognized)
+    {
+        void(^action)(void) = [JLAssociatedObjectUtils JL_getAssociatedObject:self key:kJLActionHandlerTapBlockKey];
+        
+        if (action)
+        {
+            action();
+        }
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
